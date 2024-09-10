1b01847f191c6c5d2d64b65287190d1240d504d9
slucaci <serjlucaci@gmail.com>
2024-09-10 09:10:58 +0100

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,41 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node arrow-functions.js in the terminal
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//   //Code Block
+//   return a + b;
+// }
+
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+  //Code Block
+  return a + b;
+};
 
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b);=>(a+b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
 // Implicit Returns
 
+const saySomething = (message) => console.log(message);
+saySomething("Hello there!!");
+const sayHello = () => console.log("hello");
+sayHello();
 // Returning Multiple Lines
+
+const returnMultipleLines = () =>
+  `<p>
+    This is a multiline string!
+    </p>`;
+console.log(returnMultipleLines());
